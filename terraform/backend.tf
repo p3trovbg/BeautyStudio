8cbaa957d6265f2156ef6e06116cc0d1b7963254
Georgi Petrov <78408524+p3trovbg@users.noreply.github.com>
2026-02-28 10:50:35 +0200

    ci: configure Terraform Cloud org (Petrov_Gergevec) and make kubeconfig optional

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   cloud {
     # Replace "your-org" with your Terraform Cloud organization name.
     # Sign up for free at https://app.terraform.io
-    organization = "your-org"
+    organization = "Petrov_Gergevec"
 
     workspaces {
       name = "beauty-studio"
